870f378d33d4ff652f763349afb05b62464541ea
Azat-jyrgalbek-uulu <azat.jyrgalbekuulu.alt@gmail.com>
2026-05-18 22:05:01 +0600

    first commit

---
+++ BankSystem 4.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankSystem_4._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
