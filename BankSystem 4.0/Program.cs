b91e03a262f31eacae522eb1c81b61542007fb86
Azat-jyrgalbek-uulu <azat.jyrgalbekuulu.alt@gmail.com>
2026-05-18 22:19:28 +0600

    add first class

--- BankSystem 4.0/Program.cs
+++ BankSystem 4.0/Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace BankSystem_4._0
 {
+    class BankAccount
+    {
+        private string Name;
+        private uint Id;
+        private decimal Balance;
+    }
     internal class Program
     {
         static void Main(string[] args)
