c4a2c097cfccac36a4471866658aba0d356fac00
Azat-jyrgalbek-uulu <azat.jyrgalbekuulu.alt@gmail.com>
2026-05-19 07:30:48 +0600

    add createaccount button

--- BankSystem 4.0/Program.cs
+++ BankSystem 4.0/Program.cs
@@ -2,15 +2,48 @@
 {
     class BankAccount
     {
-        private string Name;
-        private uint Id;
-        private decimal Balance;
+        public string Name;
+        public int Id;
+        public decimal Balance {  get; set; }
     }
     internal class Program
     {
+        static BankAccount CreateAccount(string name, int id)
+        {
+            return new BankAccount { Name = name, Id = id };
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<BankAccount> accounts = new List<BankAccount>();
+
+            while (true)
+            {
+                int.TryParse(Console.ReadLine(), out int button);
+                if (button == 1)
+                {
+                    Console.WriteLine("Введите имя");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите ID");
+                    int.TryParse(Console.ReadLine(), out int id);
+                    accounts.Add(CreateAccount(name, id));
+                }
+                else if (button == 2)
+                {
+                    Console.WriteLine("Посмотреть аккаунты");
+
+                    foreach (var account in accounts)
+                    {
+                        Console.WriteLine($"{account.Name}, Баланс: {account.Balance}");
+                    }
+                }
+                else if(button == 0)
+                {
+                    break;
+                }
+
+                Console.ReadLine();
+                  
+            }
         }
     }
 }
