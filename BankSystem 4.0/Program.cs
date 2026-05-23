23b65d201c3ec535466a369c93fa17f5db598d5f
Azat-jyrgalbek-uulu <azat.jyrgalbekuulu.alt@gmail.com>
2026-05-23 22:31:54 +0600

    Добавил историю операций аккаунта и перевод средств между аккаунтами. Рефакторинг кода, вынес проверки в методы.Улучшил проверки имени.

--- BankSystem 4.0/Program.cs
+++ BankSystem 4.0/Program.cs
@@ -5,19 +5,53 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Balance { get; set; }
+
+        List<HistoryOfOperation> OperationHistory = new List<HistoryOfOperation>();
         public override string ToString()
         {
             return $"{Name}, {Id}, Баланс: {Balance}";
         }
-        public void Deposite(decimal amount)
+        public void Deposit(decimal amount)
         {
-            Balance += amount;
-        }
-        public void Withdrow (decimal amount)
-        {
-            Balance -= amount;
-        }
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Сумма пополнения не может быть меньше или равно 0");
+            }
 
+            Balance += amount;
+
+            OperationHistory.Add(new HistoryOfOperation
+            {
+                TypeOfOperation = "Пополнение",
+                AmountOfOperation = amount
+            });
+        }
+        public void Withdraw (decimal amount, string type)
+        {
+            if (amount > Balance)
+            {
+                throw new ArgumentException("Недостаточно средств");
+            }
+            else if (amount <= 0)
+            {
+                throw new ArgumentException("Сумма не может быть меньше или равно 0");
+            }
+
+            Balance -= amount;
+
+            OperationHistory.Add(new HistoryOfOperation
+            {
+                TypeOfOperation = type,
+                AmountOfOperation = amount
+            });
+        }
+        public void ShowHistory ()
+        {
+            foreach ( var operation in OperationHistory )
+            {
+                Console.WriteLine(operation);
+            }
+        }
     }
     class Bank
     {
@@ -46,8 +80,69 @@
         }
         
     }
+
+    class HistoryOfOperation
+    {
+        public string TypeOfOperation { get; set; }
+        public decimal AmountOfOperation { get; set; }
+
+        public override string ToString()
+        {
+            return $"{TypeOfOperation}: {AmountOfOperation}";
+        }
+    }
     internal class Program
     {
+        static int ReadId ()
+        {
+            Console.WriteLine("Введите ID");
+
+            while (true)
+            {
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userId);
+
+                if (!isNumber)
+                {
+                    Console.WriteLine("ID должен быть числовым, попробуйте еще раз");
+                    continue;
+                }
+
+                return userId;
+            }
+        }
+        static decimal ReadAmount()
+        {
+            Console.WriteLine("Введите сумму");
+
+            while (true)
+            {
+                bool isNumber = decimal.TryParse(Console.ReadLine(),out decimal userAmount);
+
+                if (!isNumber)
+                {
+                    Console.WriteLine("Сумма должна быть числом");
+                    continue;
+                }
+
+                return userAmount;
+            }
+        }
+        static BankAccount GetAccount (Bank bank)
+        {
+            while (true)
+            {
+                var foundAccount = bank.FindAccount(ReadId());
+
+                if (foundAccount == null)
+                {
+                    Console.WriteLine("Аккаунт не найден, попробуйте еще раз");
+                    continue;
+                }
+
+                return foundAccount;
+            }
+
+        }
         static void Main(string[] args)
         {
             Bank someBank = new Bank ();
@@ -55,10 +150,14 @@
             
             while (true)
             {
+                Console.Clear ();
                 Console.WriteLine(
                     "1 - Создать аккаунт\n" +
                     "2 - Посмотреть аккаунты\n" +
-                    "3 - Пополнить баланс");
+                    "3 - Пополнить баланс\n" +
+                    "4 - Cнять средства\n" +
+                    "5 - История операций аккаунта\n" +
+                    "6 - Перевод средств");
 
                 if (!int.TryParse(Console.ReadLine(), out int button))
                 {
@@ -71,127 +170,166 @@
                     case 1:
                         {
                             Console.WriteLine("Введите свое имя");
-                            string customerName = Console.ReadLine();
-                            Console.WriteLine("Введите ID");
                             bool isRunning = true;
                             while (isRunning)
                             {
-                                bool isNumber = int.TryParse(Console.ReadLine(), out int id);
+                                string name = Console.ReadLine();
 
-                                if (!isNumber)
+                                if (name.Length < 3)
                                 {
-                                    Console.WriteLine("Введите число");
+                                    Console.WriteLine("Имя должно быть из не менее трех букв");
                                     continue;
                                 }
 
-                                someBank.CreateAccount(customerName, id);
+                                if(string.IsNullOrWhiteSpace(name))
+                                {
+                                    Console.WriteLine("Имя не может пустым или содержать пробелы");
+                                    continue;
+                                }
 
-                                Console.WriteLine("Аккаунт создан");
+                                bool doesContainDigit = false;
+
+                                foreach (char letter in name)
+                                {
+                                    if (char.IsDigit(letter))
+                                    {
+                                        doesContainDigit = true;
+                                    }
+                                
+                                }
+
+                                if (doesContainDigit)
+                                {
+                                    Console.WriteLine("Имя содержит цифру, попробуйте еще раз");
+                                    continue;
+                                }
+
+                                bool isSymbol = false;
+
+                                foreach (char letter in name)
+                                {
+                                    if (!char.IsLetter(letter))
+                                    {
+                                        isSymbol = true;
+                                    }
+                                }
+
+                                if (isSymbol)
+                                {
+                                    Console.WriteLine("Имя содержит символы, попробуйте еще раз");
+                                    continue;
+                                }
+
+                                someBank.CreateAccount(name, ReadId());
+
+                                Console.WriteLine("Аккаунт создан. Нажмите Enter...");
+
+                                Console.ReadLine();
 
                                 isRunning = false;
-
                             }
-
 
                             break;
                         }
                     case 2:
                         {
                             someBank.ShowInfo();
+
+                            Console.WriteLine("Нажмите на Enter...");
+
+                            Console.ReadLine();
+
                             break;
                         }
                     case 3:
                         {
-                            Console.WriteLine("Для пополнения баланс введите ID");
+                            Console.WriteLine("Пополнение баланса");
 
-                            bool isRunning = true;
+                            var foundUser = GetAccount(someBank);
 
-                            while (isRunning)
+                            decimal userAmount = ReadAmount();
+
+                            try
                             {
-                                bool isIdNumber = int.TryParse(Console.ReadLine(), out int id);
+                                foundUser.Deposit(userAmount);
 
-                                if (!isIdNumber)
-                                {
-                                    Console.WriteLine("Введите число");
-                                    continue;
-                                }
-
-                                var foundUser = someBank.FindAccount(id);
-
-                                if (foundUser == null)
-                                {
-                                    Console.WriteLine("Аккаунт не найден");
-                                    continue;
-                                }
-
-                                Console.WriteLine("Введите сумму для пополнения");
-
-                                while(true)
-                                {
-                                    bool isAmountNumber = decimal.TryParse(Console.ReadLine(),out decimal amount);
-
-                                    if (!isAmountNumber)
-                                    {
-                                        Console.WriteLine("Ожидается число");
-                                        continue;
-                                    }
-
-                                    foundUser.Deposite(amount);
-
-                                    Console.WriteLine($"Баланс пополнен: {amount}");
-
-                                    break;
-                                }
-
-                                isRunning = false;
-                                
+                                Console.WriteLine($"Баланс пополнен: {userAmount}");
+                            }
+                            catch(ArgumentException ex)
+                            {
+                                Console.WriteLine(ex.Message);
                             }
 
-                           
+                            Console.WriteLine("Нажмите на Enter...");
+                            Console.ReadLine();
+
                             break;
                         }
                     case 4:
                         {
-                            Console.WriteLine("Для списания введите ID");
+                            string type = "Списание";
 
-                            while (true)
+                            Console.WriteLine("Cписание средств");
+
+                            var foundUser = GetAccount(someBank);
+
+                            decimal userAmount = ReadAmount();
+
+                            try
                             {
-                                bool isIdNumber = int.TryParse(Console.ReadLine(), out int id);
+                                foundUser.Withdraw(userAmount, type);
+                                Console.WriteLine($"{type}: {userAmount}");
 
-                                if (!isIdNumber)
-                                {
-                                    Console.WriteLine("ID долже быть числовым");
-                                    continue;
-                                }
-
-                                var foundUser = someBank.FindAccount(id);
-
-                                if(foundUser == null)
-                                {
-                                    Console.WriteLine("Аккаунт не найден");
-                                    continue;
-                                }
-
-                                Console.WriteLine("Введите сумму списания");
-
-                                while (true)
-                                {
-                                    bool isAmountNumber = decimal.TryParse(Console.ReadLine(), out decimal  amount);
-
-                                    if(!isAmountNumber)
-                                    {
-                                        Console.WriteLine("Сумма должна быть числовым");
-                                        continue;
-                                    }
-
-                                    foundUser.Withdrow(amount);
-
-                                    Console.WriteLine($"Cписание: {amount}");
-                                    break;
-                                }
-
-                                break ;
                             }
+                            catch (ArgumentException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+
+                            Console.WriteLine("Нажмите на Enter...");
+
+                            Console.ReadLine ();
+
+                            
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Посмотреть историю операций");
+
+                            var foundUser = GetAccount(someBank);
+
+                            foundUser.ShowHistory();
+
+                            Console.WriteLine("\nНажмите на Enter...");
+
+                            Console.ReadLine();
+                            
+                            break;
+                        }
+                    case 6:
+                        {
+                            string type = "Перевод средств";
+
+                            Console.WriteLine("Перевод денег по ID");
+
+                            var foundUser = GetAccount(someBank);
+
+                            Console.WriteLine("ID получателя");
+
+                            var receiver = GetAccount(someBank);
+
+                            decimal userAmount = ReadAmount();
+
+                            foundUser.Withdraw(userAmount, type);
+
+                            receiver.Deposit(userAmount);
+
+                            Console.WriteLine($"Cписание: {userAmount}");
+
+                            Console.WriteLine("\nНажмите на Enter...");
+
+                            Console.ReadLine();
 
                             break;
                         }
